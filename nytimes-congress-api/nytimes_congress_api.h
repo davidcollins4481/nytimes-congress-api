ed89763bebe3335b6de6b64e9ebc27a117a619bc
David Collins <davidcollins4481@gmail.com>
2013-08-31 17:16:59 -0400

    Initial Commit

---
+++ nytimes-congress-api/nytimes_congress_api.h
@@ -0,0 +1,13 @@
+//
+//  nytimes_congress_api.h
+//  nytimes-congress-api
+//
+//  Created by David Collins on 8/31/13.
+//  Copyright (c) 2013 David Collins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface nytimes_congress_api : NSObject
+
+@end
---
+++ nytimes-congress-api/nytimes_congress_api.m
@@ -0,0 +1,13 @@
+//
+//  nytimes_congress_api.m
+//  nytimes-congress-api
+//
+//  Created by David Collins on 8/31/13.
+//  Copyright (c) 2013 David Collins. All rights reserved.
+//
+
+#import "nytimes_congress_api.h"
+
+@implementation nytimes_congress_api
+
+@end
